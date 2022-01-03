edee60d278d2c153e730b34861f2e6a3c45d3daa
dobik <dawid.dobosz.stud@pw.edu.pl>
2022-01-03 20:18:08 +0100

    testowy

---
+++ test.c
@@ -0,0 +1,14 @@
+int k = 15;
+char * z= "s";
+char *funk(char d, char *b);
+char *funk(char d, char *b){
+    char *napi;
+    int k;
+    printf("plik");
+    return "napis";
+}
+int main(int argc, char **argv){
+    double z = 15.0;
+}
+
+
